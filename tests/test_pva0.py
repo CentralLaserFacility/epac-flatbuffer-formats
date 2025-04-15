2dfc0e074748fa0fdeeddd62aa758da66402b8aa
Anuj Mittal <anuj.mittal@stfc.ac.uk>
2025-04-15 11:09:02 +0530

    add waveform pva0 tests

--- tests/test_pva0.py
+++ tests/test_pva0.py
@@ -130,6 +130,16 @@ class TestSerialisationPVA0:
         assert nt_ndarray_obj.model_dump()["display"]["form"] == 0
         assert np.array_equal(nt_ndarray_dict["value"], nt_ndarray_obj.value)
 
+    def test_serialises_and_deserialises_waveform_correctly(self, nt_scalar_dict):
+
+        nt_scalar_obj = dt.NTScalarAny(**nt_scalar_dict)
+        waveform_obj = dt.Waveform(dataX=nt_scalar_obj, dataY=nt_scalar_obj)
+        pv_data_obj = dt.PVData(data=waveform_obj, source_name=self.source_name)
+        buf = serialise_data(pv_data_obj)
+        assert isinstance(buf, bytes)
+        deserialised_obj = deserialise_data(buf)
+        assert deserialised_obj == pv_data_obj
+
     def test_serialises_and_deserialises_scalar_ntscalarany_correctly(
         self, nt_scalar_dict
     ):
